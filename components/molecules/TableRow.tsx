c6fdee6174ba139a71a01cb95bb389fa69a9e2b4
alfanjuni <alfanjuni@gmail.com>
2025-08-16 16:14:06 +0700

    strong buy sell

--- components/molecules/TableRow.tsx
+++ components/molecules/TableRow.tsx
@@ -79,12 +79,22 @@ export function TableRow({ symbol, index }: TableRowProps) {
     symbol: CryptoSymbol
   ): "buy" | "sell" | "hold" | "strong buy" | "strong sell" => {
     //  RSI HTF > 50 + Stoch Oversold + RSI < 30 → STRONG BUY
-    if (symbol.rsiHTF > 50 && symbol.slowK < 20 && symbol.slowD < 20) {
+    if (
+      symbol.rsiHTF > 50 &&
+      symbol.slowK < 20 &&
+      symbol.slowD < 20 &&
+      symbol.rsi < 30
+    ) {
       return "strong buy";
     }
 
     // RSI HTF < 50 + Stoch Overbought + RSI > 70 → STRONG SEL
-    if (symbol.rsiHTF < 50 && symbol.slowK > 80 && symbol.slowD > 80) {
+    if (
+      symbol.rsiHTF < 50 &&
+      symbol.slowK > 80 &&
+      symbol.slowD > 80 &&
+      symbol.rsi > 70
+    ) {
       return "strong sell";
     }
 
