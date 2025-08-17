2a1649ad6d660fadce904f6b7cbf615f939a11e5
alfanjuni <alfanjuni@gmail.com>
2025-08-17 14:48:58 +0700

    fix buy rsi

--- lib/indicators/signal.ts
+++ lib/indicators/signal.ts
@@ -55,12 +55,12 @@ export const getSignal = (
   }
 
   // RSI HTF > 50 + Stoch Oversold → BUY
-  if (symbol.rsiHTF > 50 && symbol.slowK < 20 && symbol.slowD < 20) {
+  if (symbol.rsiMTF > 50 && symbol.slowK < 20 && symbol.slowD < 20) {
     return "buy";
   }
 
   // RSI HTF < 50 + Stoch Overbought → SELL
-  if (symbol.rsiHTF < 50 && symbol.slowK > 80 && symbol.slowD > 80) {
+  if (symbol.rsiMTF < 50 && symbol.slowK > 80 && symbol.slowD > 80) {
     return "sell";
   }
 
